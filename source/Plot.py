59b56613da3712d999dda125f91682a56dc6a53d
ahmedasad236 <68563546+ahmedasad236@users.noreply.github.com>
2021-12-25 00:11:28 +0200

    Final Plot.py with validations

--- source/Plot.py
+++ source/Plot.py
@@ -1,82 +1,42 @@
 import matplotlib.pyplot as plt 
 import numpy as np
+from Validation import Validation
 import re
 import sys
 
-
-
 class Plot:
     def __init__(self, exp, minVal, maxVal):
-        self.expression = self.validateExpresion(exp)
-        self.minValue = self.validateInteger(minVal)
-        self.maxValue = self.validateInteger(maxVal)
-        self.validateMaxMinValues()
-        self.plotFunction()
-    
-    def validateExpresion(self, expression):
-        expression.replace(' ', '')
-        if expression == "":
-            raise ValueError("Error: Empty Function")
-        
-        toMatch = "(-)?((\d+[\*+-\/])?[xX](\^\d+)?([+-]\d([\*\/][xX](\^\d+)?)?)*)*$"
-        matched = re.match(toMatch, expression)
-        
-        if not matched:
-            raise ValueError("Invalid Expression")
-        
-        expression = expression.replace('^', '**').replace('X', 'x')
-        return expression
-    
-    def validateInteger(self, strNum):
-        try:
-            num = int(strNum)
-            return num
-        except:
-            raise TypeError("Error: Not integer")
-    
-    def validateMaxMinValues(self):
-        if self.minValue >= self.maxValue:
-            raise ValueError("Maximum must be greater than Minimum")
-    
+        self.validate = Validation()
+        self.expression = self.validate.validateExpresion(exp)
+        print(self.expression)
+        self.minValue = self.validate.validateInteger(minVal)
+        self.maxValue = self.validate.validateInteger(maxVal)
+        self.validate.validateMaxMinValues(self.minValue, self.maxValue)
+#-------------------------------------------------------------------------------------    
     def F(self, x):
         val = eval(self.expression)
         return val
-        
-        
+#-------------------------------------------------------------------------------------
     def generateFunction(self):
         xList = []
         yList = []
+        testZeroDiv = self.validate.validateDivisionByZero(self.expression, self.minValue)
         for i in range(self.minValue, self.maxValue):
             xList.append(i)
-            yList.append(self.F(i))
+            if testZeroDiv == False and i == 0: #put the y-value with infinity if(x is a denominator and x == 0)
+                yList.append(float('inf'))
+            else:    
+                yList.append(self.F(i))
         
         return xList, yList
-        
+#-------------------------------------------------------------------------------------
     def plotFunction(self):
         xList, yList = self.generateFunction()
-        plt.plot(xList, yList)
-        #plt.title("F(X) = ", self.expression)
+        plt.plot(xList, yList, color="red", linewidth=1.5, label=self.expression)
         plt.xlabel("X")
         plt.ylabel("F(X)")
-        plt.style.use("seaborn-whitegrid")
+        plt.style.use("seaborn-dark")
         plt.show()
-
-
-# fun = input("Enter the input equation: ")
-# mn= int(input("Enter min val: "))
-# mx = int(input("Enter max val: "))
-
-
-# def f(x):
-#     val= (eval(fun.replace('^', '**'))); 
-#     return val
-# x= []
-# y = []
-# for i in range(mn, mx):
-#     x.append(i)
-#     y.append(f(i))
-    
-# plt.plot(x, y);
-# plt.show();
+#-------------------------------------------------------------------------------------
 
 
