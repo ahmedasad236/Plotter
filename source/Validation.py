81fbcdb839770407cbd2e4eb2c6a1f6861382c5c
ahmedasad236 <ahmedasad236@gmail.com>
2021-12-25 00:24:06 +0200

    Add class Validation to check all validations

---
+++ source/Validation.py
@@ -0,0 +1,36 @@
+import re
+import sys
+
+class Validation:
+    
+    def validateExpresion(self, expression):
+        expression = expression.replace(" ", "")
+        if expression == "":
+            raise ValueError("Empty Function")
+        
+        toMatch = "(-)?(\d+$)|((\d+[\*+-\/])?[xX](\^\d+)?([+-]\d+([\*\/][xX](\^\d+)?)?)*)*$"
+        matched = re.match(toMatch, expression)
+        if not matched:
+            raise ValueError("Invalid Expression")
+        
+        expression = expression.replace('^', '**').replace('X', 'x')
+        
+        return expression
+#-------------------------------------------------------------------------------------
+    def validateInteger(self, strNum):
+        try:
+            num = int(strNum)
+            return num
+        except:
+            raise ValueError("Not a range")
+#-------------------------------------------------------------------------------------    
+    def validateMaxMinValues(self, minValue, maxValue):
+        if minValue >= maxValue:
+            raise ValueError("Maximum must be greater than Minimum")
+#-------------------------------------------------------------------------------------
+    def validateDivisionByZero(self, expression, minValue):
+        if expression.find('/X') != -1 or expression.find('/x') != -1 and minValue <= 0:
+            return False
+        return True
+
+
